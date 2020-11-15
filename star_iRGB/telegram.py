f175bbd3012b6dfad84efa9bb07a79a74e268f34
clebeson <clebeson_ufs@hotmail.com>
2020-11-15 19:43:05 -0300

    Adding phd experiment codes

--- star_iRGB/telegram.py
+++ star_iRGB/telegram.py
@@ -4,8 +4,8 @@ import requests
 
 def telegram_bot_sendtext(bot_message):
     print("sending ...")
-    bot_token = '754818149:AAGlBDM_u5xovmeM8liGh3qpIQHq8IFNIMc'
-    bot_chatID = '887550379'
+    bot_token = '*********'
+    bot_chatID = '********'
     
     # send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
